8b2ba59422923a10c9e1450295e4dace335fe0c1
EC2 Default User <ec2-user@ip-172-31-9-201.us-west-2.compute.internal>
2020-02-25 06:39:52 +0000

    changed test.sh

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -15,6 +15,6 @@ set +x
 
 echo 'The following "npm" command tests that your simple Node.js/React'
 echo 'application renders satisfactorily. This command actually invokes the test'
-echo 'runner Jest (https://facebook.github.io/jest/).'
+echo 'runner Jest (https://facebook.github.io/test/).'
 set -x
 npm test
